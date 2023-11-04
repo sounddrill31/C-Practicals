3e27eef4fa4efb26eec7699051037f2ecd4215ac
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-11-04 09:40:41 +0530

    Add files via upload

---
+++ Alph3.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+    char ch;
+    clrscr();
+    printf("Enter a Character\n");
+    scanf("%c",&ch);
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        printf("%d is a character!\n",ch);
+    }
+    else if(ch >= '0' && ch <= '9')
+    {
+        printf("%c is a digit\n",ch);
+    }
+    else
+    {
+        printf("%c is a special character\n",ch);
+    }
+}
